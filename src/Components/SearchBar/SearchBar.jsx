5f1c73e2ac523de07aef4cd42431b26b38d6bd35
WTDula <wtdula@gmail.com>
2022-04-28 11:22:11 -0500

    added functionality to SearchBar component as form submit for search for MVP, dynamic search in refactor?

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -3,27 +3,30 @@ import React, { useState } from 'react';
 const SearchBar = (props) => {
 
     const [searchField, setSearchField] = useState("")
-
-    const filteredSongs = props.songs.filter((song) => {
-        return (
-            song.title.toLowerCase().includes(searchField.toLowerCase()) ||
-            song.artist.toLowerCase().includes(searchField.toLowerCase()) ||
-            song.album.toLowerCase().includes(searchField.toLowerCase()) ||
-            song.release_date.toLowerCase().includes(searchField.toLowerCase()) ||
-            song.genre.toLowerCase().includes(searchField.toLowerCase())
-        )
-    })
+    const [searchResults, setSearchResults] = useState([])
+    
 
     function handleChange(event){
-        setSearchField(event.target.value)
-        props.setSongs(filteredSongs)
+        event.preventDefault()
+        const filteredSongs = props.songs.filter((song) => {
+            return (
+                song.title.toLowerCase().includes(searchField.toLowerCase()) ||
+                song.artist.toLowerCase().includes(searchField.toLowerCase()) ||
+                song.album.toLowerCase().includes(searchField.toLowerCase()) ||
+                song.release_date.toLowerCase().includes(searchField.toLowerCase()) ||
+                song.genre.toLowerCase().includes(searchField.toLowerCase())
+            )
+        })
+        setSearchResults(filteredSongs)
+        props.setSongs(searchResults)
     }
 
     return ( 
-        <div>
+        <form onSubmit={handleChange}>
             <h3>Search For Your Song</h3>
-            <input type="search" placeholder='search song' onChange={handleChange} />
-        </div>
+            <input type="search" placeholder='search song' id={searchField} onChange={event => setSearchField(event.target.value)}/>
+            <button type='submit'>Double click to search</button>
+        </form>
      );
 }
  
